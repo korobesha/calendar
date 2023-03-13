9f6f3d68c29fdc1b3df25c1181f2aa227ea0aae7
korobesha <korobeynikov92@gmail.com>
2023-03-13 18:26:49 +0500

    TASK-10

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,23 +1,31 @@
 <template>
   <div>
     <router-view></router-view>
-  <div>
-    <input type="checkbox" id="hmt" class="hidden-menu-ticker">
-    <label class="btn-menu" for="hmt">
-  <span class="first"></span>
-  <span class="second"></span>
-  <span class="third"></span>
-</label>
-
-<ul class="hidden-menu">
-  <li><router-link class="link" :to="{name:'calendar'}">Calendar
-  </router-link></li>  
-  <li><router-link class="link" :to="{name:'weather'}">Weather
-  </router-link></li>
-  <li><router-link class="link" :to="{name:'main'}">Main
-  </router-link></li>  
-</ul>
-</div>
+    <div>
+      <input type="checkbox" id="hmt" class="hidden-menu-ticker">
+      <label class="btn-menu" for="hmt">
+        <span class="first"></span>
+        <span class="second"></span>
+        <span class="third"></span>
+      </label>
+      <ul class="hidden-menu">
+        <li>
+          <router-link class="link" :to="{name:'calendar'}">
+            Calendar
+          </router-link>
+        </li>  
+        <li>
+          <router-link class="link" :to="{name:'weather'}">
+            Weather
+          </router-link>
+        </li>
+        <li>
+          <router-link class="link" :to="{name:'main'}"
+          >Main
+          </router-link>
+        </li>  
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -32,11 +40,13 @@ export default {
   font-family: 'Roboto', sans-serif;
   font-size: 14px;
   color: black;
-text-decoration: none;
-:visited{
+  text-decoration: none;
+
+  &:visited {
   color: black;
-}
+  }
 };
+
 .hidden-menu {
   display: block;
   position: fixed;
@@ -49,7 +59,7 @@ text-decoration: none;
   height: 100%;
   top: 0;
   right: -200px;
-  transition: left .2s;
+  transition: left 1.5s;
   z-index: 2;
   transform: translateZ(0);
   backface-visibility: hidden;
@@ -62,26 +72,28 @@ text-decoration: none;
 .btn-menu {
   color: #fff;
   background-color: #000;
-  padding: 7px;
+  padding: 5px;
   position: fixed;
   top: 5px;
   right: 5px;
   cursor: pointer;
-  transition: right .23s;
+  transition: left 1.5s;
   z-index: 3;
   width: 25px;
   transform: translateZ(0);
   backface-visibility: hidden;
 };
+
 .btn-menu span {
   display: block;
   right: 0;
   height: 3px;
   background-color: #fff;
   margin: 5px 0 0;
-  transition: all .1s linear .23s;
+  transition: all .1s linear .2s;
   position: relative;
 };
+
 .btn-menu span.first {
   margin-top: 0;
 };
@@ -89,19 +101,23 @@ text-decoration: none;
 .hidden-menu-ticker:checked ~ .btn-menu {
   right: 5px;
 };
+
 .hidden-menu-ticker:checked ~ .hidden-menu {
   right: 0;
 };
+
 .hidden-menu-ticker:checked ~ .btn-menu span.first {
   transform: rotate(45deg);
-  top: 8px;
-  
+  top: 8px; 
 };
+
 .hidden-menu-ticker:checked ~ .btn-menu span.second {
   opacity: 0;
 };
+
 .hidden-menu-ticker:checked ~ .btn-menu span.third {
   transform: rotate(-45deg);
   top: -8px;    
 };
+
 </style>
