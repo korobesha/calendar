44aa812a20be93d004b6a21951c3c182f451f6e4
korobesha <korobeynikov92@gmail.com>
2023-03-16 17:28:13 +0500

    TASK-12

--- src/pages/CalendarBlock.vue
+++ src/pages/CalendarBlock.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="calendar-block">
-    <LoaderRing v-if="loading"/>
-    <div v-else>
       <CalendarHeader class="calendar-block-header" />
       <CalendarYear />
-    </div>
   </div>
 </template>
 
 <script>
 import CalendarYear from '@/components/calendar/CalendarYear.vue';
 import CalendarHeader from '@/components/calendar/CalendarHeader.vue';
-import LoaderRing from '@/components/LoaderRing.vue';
+
 
 export default {
   name: 'CalendarBlock',
   components: {
     CalendarYear,
     CalendarHeader,
-    LoaderRing
   }, 
-  data: () => ({
-    loading: true,
-  }),
-  mounted() {
-    setTimeout(() => {
-      this.loading = false}, 1000)
-  }
 }
 </script>
 
--- src/pages/MainBlock.vue
+++ src/pages/MainBlock.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    main
+  <div class="main-block">
+    <PageLoader v-if="isLoading"/>
+      <template v-else>
+        {{ title }}
+      </template>
   </div>
 </template>
 
 <script>
+import PageLoader from '@/components/PageLoader.vue';
+
 export default {
   name: 'MainBlock',
   components: {
+    PageLoader,
   },  
+  data: () => ({
+    isLoading: true,
+    title: '',
+  }),
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json =>
+        setTimeout(() => {
+          this.title=json.title;
+          console.log(json.title);
+          this.isLoading = false;
+        }, 1000) 
+      )
+  }
 }
 </script>
 
